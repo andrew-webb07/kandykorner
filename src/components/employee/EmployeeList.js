0291ccebcb3cc0285c69d0444230777776750e2d
Andrew Webb <webba@ihopu.org>
2021-06-07 14:52:34 -0500

    created employee form

---
+++ src/components/employee/EmployeeList.js
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect } from "react"
+// import { useHistory } from "react-router"
+import {EmployeeContext } from "./EmployeeProvider"
+import { useHistory } from 'react-router-dom';
+import "./Employee.css"
+
+export const EmployeeList = () => {
+    const { getEmployees, employees } = useContext(EmployeeContext)
+
+    useEffect(() => {
+        getEmployees()
+    }, [])
+
+    const history = useHistory()
+
+    return (
+        <>
+        <h1>Kandy Employees</h1>
+
+        <button className="hireButton" onClick={() => history.push("/employees/create")}>
+            Hire Employee
+        </button>
+
+        <div className="employees">
+            {
+                employees.map(employee => 
+                    <>
+                    <div className="employee">
+                        <h3>{employee.name}</h3>
+                        <div>{employee.location.name}</div>
+                        <div>{employee.isManager ? "Is a Manager" : ""}</div>
+                        <div>{employee.fullTime ? "Is Full Time" : ""}</div>
+                        <div>Makes ${employee.hourlyRate} per hour</div>
+                    </div>
+                    </>
+                    )
+            }
+        </div>
+        </>
+    )
+}
